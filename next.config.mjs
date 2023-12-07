66e883deaffbd8e5ba1783afde9ea732a9f1d620
Sarthak Chawla <sarthak.chawla@agoda.com>
2023-12-07 23:26:28 +0530

    Fix next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  experimental: {
-    mdxRs: true,
-  },
-  // reactStrictMode: true,
-  // // Add basePath
-  // basePath: "/ITKS",
+  // experimental: {
+  //   mdxRs: true,
+  // },
+  reactStrictMode: true,
+  // Add basePath
+  basePath: "/ITKS",
 };
 
 export default withContentlayer(nextConfig);
