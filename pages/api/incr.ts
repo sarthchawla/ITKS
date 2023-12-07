15e6591a92aec2647735998602979c7fcace128a
Sarthak Chawla <sarthak.chawla@agoda.com>
2023-12-07 23:58:16 +0530

    Remove unused dependencies and commented out code

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -1,7 +1,7 @@
-import { Redis } from "@upstash/redis";
+// import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
 
-const redis = Redis.fromEnv();
+// const redis = Redis.fromEnv();
 export const config = {
   runtime: "edge",
 };
@@ -33,15 +33,15 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
       .map((b) => b.toString(16).padStart(2, "0"))
       .join("");
 
-    // deduplicate the ip for each slug
-    const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
-      nx: true,
-      ex: 24 * 60 * 60,
-    });
-    if (!isNew) {
-      new NextResponse(null, { status: 202 });
-    }
+    // // deduplicate the ip for each slug
+    // const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
+    //   nx: true,
+    //   ex: 24 * 60 * 60,
+    // });
+    // if (!isNew) {
+    //   new NextResponse(null, { status: 202 });
+    // }
   }
-  await redis.incr(["pageviews", "projects", slug].join(":"));
+  // await redis.incr(["pageviews", "projects", slug].join(":"));
   return new NextResponse(null, { status: 202 });
 }
