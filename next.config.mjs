b67671995c5727ddecb2a8177cb5ef577ebff30c
Sarthak Chawla <sarthak.chawla@agoda.com>
2023-12-07 23:12:42 +0530

    next js app init

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	experimental: {
+		mdxRs: true,
+	},
+	// reactStrictMode: true,
+	 // Add basePath
+	//  basePath: '/github-pages',
+};
+
+export default withContentlayer(nextConfig);
